88af06f0a547da209ba2264d751fc49963b42be7
Julian Grinblat <julian@dotcore.co.il>
2026-07-14 01:03:28 +0900

    chore!: replace mocha with node:test

--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
 import { join } from "node:path";
-import { readFileSync } from "node:fs";
+import { readFile } from "node:fs/promises";
 import { expect } from "chai";
 import { requireFromString, importFromStringSync } from "module-from-string";
 
@@ -8,75 +8,70 @@ import { requireFromString, importFromStringSync } from "module-from-string";
 import lua2js from "redis-lua2js";
 
 describe("redis-lua2js", () => {
-  it("should export name and numberOfKeys from lua", () => {
-    const lua = readFileSync(
+  it("should export name and numberOfKeys from lua", () =>
+    readFile(
       // eslint-disable-next-line n/no-unsupported-features/node-builtins
       join(import.meta.dirname, "test_both.lua"),
       "utf8",
-    );
-
-    expect(requireFromString(lua2js(lua))).to.deep.equal({
-      name: "pdel",
-      numberOfKeys: 1,
-      lua,
-    });
-  });
-
-  it("should return null name and numeberOfKeys", () => {
-    const lua = readFileSync(
-      // eslint-disable-next-line n/no-unsupported-features/node-builtins
-      join(import.meta.dirname, "test_none.lua"),
-      "utf8",
-    );
-
-    expect(requireFromString(lua2js(lua))).to.deep.equal({
-      name: null,
-      numberOfKeys: null,
-      lua,
-    });
-  });
-
-  it("should export name from lua and null numberOfKeys", () => {
-    const lua = readFileSync(
-      // eslint-disable-next-line n/no-unsupported-features/node-builtins
-      join(import.meta.dirname, "test_name.lua"),
-      "utf8",
-    );
-
-    expect(requireFromString(lua2js(lua))).to.deep.equal({
-      name: "pdel",
-      numberOfKeys: null,
-      lua,
-    });
-  });
-
-  it("should export null name and numberOfKeys from lua", () => {
-    const lua = readFileSync(
-      // eslint-disable-next-line n/no-unsupported-features/node-builtins
-      join(import.meta.dirname, "test_nkeys.lua"),
-      "utf8",
-    );
-
-    expect(requireFromString(lua2js(lua))).to.deep.equal({
-      name: null,
-      numberOfKeys: 1,
-      lua,
-    });
-  });
-
-  it("should generate esm module", () => {
-    const lua = readFileSync(
-      // eslint-disable-next-line n/no-unsupported-features/node-builtins
-      join(import.meta.dirname, "test_both.lua"),
-      "utf8",
-    );
-
-    expect(importFromStringSync(lua2js(lua, { type: "module" }))).to.deep.equal(
-      {
+    ).then((lua) =>
+      expect(requireFromString(lua2js(lua))).to.deep.equal({
         name: "pdel",
         numberOfKeys: 1,
         lua,
-      },
-    );
-  });
+      }),
+    ));
+
+  it("should return null name and numeberOfKeys", () =>
+    readFile(
+      // eslint-disable-next-line n/no-unsupported-features/node-builtins
+      join(import.meta.dirname, "test_none.lua"),
+      "utf8",
+    ).then((lua) =>
+      expect(requireFromString(lua2js(lua))).to.deep.equal({
+        name: null,
+        numberOfKeys: null,
+        lua,
+      }),
+    ));
+
+  it("should export name from lua and null numberOfKeys", () =>
+    readFile(
+      // eslint-disable-next-line n/no-unsupported-features/node-builtins
+      join(import.meta.dirname, "test_name.lua"),
+      "utf8",
+    ).then((lua) =>
+      expect(requireFromString(lua2js(lua))).to.deep.equal({
+        name: "pdel",
+        numberOfKeys: null,
+        lua,
+      }),
+    ));
+
+  it("should export null name and numberOfKeys from lua", () =>
+    readFile(
+      // eslint-disable-next-line n/no-unsupported-features/node-builtins
+      join(import.meta.dirname, "test_nkeys.lua"),
+      "utf8",
+    ).then((lua) =>
+      expect(requireFromString(lua2js(lua))).to.deep.equal({
+        name: null,
+        numberOfKeys: 1,
+        lua,
+      }),
+    ));
+
+  it("should generate esm module", () =>
+    readFile(
+      // eslint-disable-next-line n/no-unsupported-features/node-builtins
+      join(import.meta.dirname, "test_both.lua"),
+      "utf8",
+    ).then((lua) =>
+      expect(
+        importFromStringSync(lua2js(lua, { type: "module" })),
+      ).to.deep.equal({
+        name: "pdel",
+        numberOfKeys: 1,
+        lua,
+      }),
+    ));
 });
